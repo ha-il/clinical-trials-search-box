b0fd6da85d76a3b59bf1669aaaf8c177c7ceaefd
HyeongWoo Kim <hyeongwookim.dev@gmail.com>
2023-09-06 16:55:09 +0900

    fix: 검색 바 포커스 이벤트 발생 시 검색 창 렌더링되도록 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 import { styled } from 'styled-components';
 
+const RECOMENDED_KEYWORDS = ['B형 간염', '비만', '관절염', '우울증', '식도염'];
+
 function App() {
+  const [isSearchBarFocused, setIsSearchBarFocused] = useState(false);
+
+  const handleFocusInput = () => setIsSearchBarFocused(true);
+  const handleBlurInput = () => setIsSearchBarFocused(false);
+
   return (
     <div className='App'>
       <Title>
         국내 모든 임상시험 검색하고 <br /> 온라인으로 참여하기
       </Title>
-      <SearchBar>
-        <input type='text' placeholder='질환명을 입력해 주세요.' />
+      <SearchBar $isSearchBarFocused={isSearchBarFocused}>
+        <input
+          type='text'
+          placeholder='질환명을 입력해 주세요.'
+          onFocus={handleFocusInput}
+          onBlur={handleBlurInput}
+        />
         <button type='button'>
           <ScopeIcon>
             <svg
@@ -24,6 +36,22 @@ function App() {
           </ScopeIcon>
         </button>
       </SearchBar>
+      {isSearchBarFocused && (
+        <SerachWindow>
+          <RecentKeywordsArea>
+            <div>최근 검색어</div>
+            <div>최근 검색어가 없습니다.</div>
+          </RecentKeywordsArea>
+          <RecomendedKeywordsArea>
+            <div>추천 검색어로 검색해보세요.</div>
+            <RecomendedKeywords>
+              {RECOMENDED_KEYWORDS.map(keyword => (
+                <RecomendedKeyword>{keyword}</RecomendedKeyword>
+              ))}
+            </RecomendedKeywords>
+          </RecomendedKeywordsArea>
+        </SerachWindow>
+      )}
     </div>
   );
 }
@@ -42,13 +70,16 @@ const ScopeIcon = styled.div`
   padding: 0 6px;
 `;
 
-const SearchBar = styled.div`
+const SearchBar = styled.div<{ $isSearchBarFocused: boolean }>`
   display: flex;
   align-items: center;
   background-color: white;
   padding: 0 6px 0 0;
   border-radius: 42px;
   padding: 10px 10px 10px 24px;
+  border: ${({ $isSearchBarFocused }) =>
+    $isSearchBarFocused ? '2px solid #007be9' : '2px solid transparent'};
+
   & input {
     padding: 1px 25px 1px 2px;
     background-color: transparent;
@@ -64,4 +95,44 @@ const SearchBar = styled.div`
     background-color: #007be9;
     border: none;
   }
+`;
+
+const SerachWindow = styled.div`
+  background-color: white;
+  width: 100%;
+  padding: 20px 0;
+  border-radius: 21px;
+  position: absolute;
+  width: 509px;
+  top: 315px;
+  box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.1);
+`;
+
+const RecentKeywordsArea = styled.div`
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+  padding: 0 0 24px 24px;
+  & div:first-child {
+    margin: 0 0 24px 0;
+    color: rgba(0, 0, 0, 0.5);
+  }
+`;
+
+const RecomendedKeywordsArea = styled.div`
+  padding: 24px 0 0 24px;
+  & div:first-child {
+    margin: 0 0 24px 0;
+    color: rgba(0, 0, 0, 0.5);
+  }
+`;
+
+const RecomendedKeywords = styled.ul`
+  display: flex;
+`;
+
+const RecomendedKeyword = styled.li`
+  background-color: #eef8ff;
+  color: #007be9;
+  padding: 16px;
+  margin: 0 8px 0 0;
+  border-radius: 32px;
 `;
