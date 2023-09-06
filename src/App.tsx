4b3de6006120d505175fa6adcaa7ffce516d0ba9
HyeongWoo Kim <hyeongwookim.dev@gmail.com>
2023-09-06 18:18:35 +0900

    fix: 검색어 입력 시 API 요청하여 추천 검색어 렌더링 되도록 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './App.css';
 import { styled } from 'styled-components';
 
-const RECOMENDED_KEYWORDS = ['B형 간염', '비만', '관절염', '우울증', '식도염'];
+const DEFAULT_RECOMENDED_KEYWORDS = ['B형 간염', '비만', '관절염', '우울증', '식도염'];
+interface Sick {
+  sickCd: string;
+  sickNm: string;
+}
+
+const isEmptyString = (keyword: string) => keyword === '';
 
 function App() {
   const [isSearchBarFocused, setIsSearchBarFocused] = useState(false);
+  const [keyword, setKeyword] = useState('');
+  const [recommendedKeywords, setRecommendedKeywords] = useState<Sick[]>([]);
 
   const handleFocusInput = () => setIsSearchBarFocused(true);
   const handleBlurInput = () => setIsSearchBarFocused(false);
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => setKeyword(e.target.value);
+
+  useEffect(() => {
+    const getRecomendedKeywords = async () => {
+      const res = await fetch(`http://localhost:4000/sick?q=${keyword}`);
+      const data = await res.json();
+      console.info('calling api');
+      setRecommendedKeywords(data);
+    };
+
+    if (keyword !== '') getRecomendedKeywords();
+    if (isEmptyString(keyword)) setRecommendedKeywords([]);
+  }, [keyword]);
 
   return (
     <div className='App'>
@@ -17,11 +38,23 @@ function App() {
         국내 모든 임상시험 검색하고 <br /> 온라인으로 참여하기
       </Title>
       <SearchBar $isSearchBarFocused={isSearchBarFocused}>
+        <ScopeIcon>
+          <svg
+            viewBox='0 0 16 16'
+            fill='currentColor'
+            preserveAspectRatio='none'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path d='M6.56 0a6.56 6.56 0 015.255 10.49L16 14.674 14.675 16l-4.186-4.184A6.56 6.56 0 116.561 0zm0 1.875a4.686 4.686 0 100 9.372 4.686 4.686 0 000-9.372z'></path>
+          </svg>
+        </ScopeIcon>
         <input
           type='text'
           placeholder='질환명을 입력해 주세요.'
           onFocus={handleFocusInput}
           onBlur={handleBlurInput}
+          onChange={handleChangeInput}
+          value={keyword}
         />
         <button type='button'>
           <ScopeIcon>
@@ -38,18 +71,52 @@ function App() {
       </SearchBar>
       {isSearchBarFocused && (
         <SerachWindow>
-          <RecentKeywordsArea>
-            <div>최근 검색어</div>
-            <div>최근 검색어가 없습니다.</div>
-          </RecentKeywordsArea>
-          <RecomendedKeywordsArea>
-            <div>추천 검색어로 검색해보세요.</div>
-            <RecomendedKeywords>
-              {RECOMENDED_KEYWORDS.map(keyword => (
-                <RecomendedKeyword>{keyword}</RecomendedKeyword>
+          {!isEmptyString(keyword) && (
+            <CurrentKeyword>
+              <svg
+                viewBox='0 0 16 16'
+                fill='currentColor'
+                preserveAspectRatio='none'
+                xmlns='http://www.w3.org/2000/svg'
+              >
+                <path d='M6.56 0a6.56 6.56 0 015.255 10.49L16 14.674 14.675 16l-4.186-4.184A6.56 6.56 0 116.561 0zm0 1.875a4.686 4.686 0 100 9.372 4.686 4.686 0 000-9.372z'></path>
+              </svg>
+              <div>{keyword}</div>
+            </CurrentKeyword>
+          )}
+          <CurrentKeywordsArea>
+            <div>{isEmptyString(keyword) ? '최근 검색어' : '추천 검색어'}</div>
+            {isEmptyString(keyword) && <div>최근 검색어가 없습니다.</div>}
+            <CurrentRecommendedKeywords>
+              {recommendedKeywords.length === 0 && keyword.length !== 0 && (
+                <div>관련 검색어 없음</div>
+              )}
+              {recommendedKeywords.slice(0, 7).map(keyword => (
+                <CurrentRecommendedKeyword key={keyword.sickCd}>
+                  <svg
+                    viewBox='0 0 16 16'
+                    fill='currentColor'
+                    preserveAspectRatio='none'
+                    xmlns='http://www.w3.org/2000/svg'
+                  >
+                    <path d='M6.56 0a6.56 6.56 0 015.255 10.49L16 14.674 14.675 16l-4.186-4.184A6.56 6.56 0 116.561 0zm0 1.875a4.686 4.686 0 100 9.372 4.686 4.686 0 000-9.372z'></path>
+                  </svg>
+
+                  <div>{keyword.sickNm}</div>
+                </CurrentRecommendedKeyword>
               ))}
-            </RecomendedKeywords>
-          </RecomendedKeywordsArea>
+            </CurrentRecommendedKeywords>
+          </CurrentKeywordsArea>
+          {isEmptyString(keyword) && (
+            <RecomendedKeywordsArea>
+              <div>추천 검색어로 검색해보세요.</div>
+              <RecomendedKeywords>
+                {DEFAULT_RECOMENDED_KEYWORDS.map((keyword, idx) => (
+                  <RecomendedKeyword key={idx}>{keyword}</RecomendedKeyword>
+                ))}
+              </RecomendedKeywords>
+            </RecomendedKeywordsArea>
+          )}
         </SerachWindow>
       )}
     </div>
@@ -66,7 +133,7 @@ const Title = styled.h1`
 const ScopeIcon = styled.div`
   width: 21px;
   height: 21px;
-  color: white;
+  color: rgba(0, 0, 0, 0.1);
   padding: 0 6px;
 `;
 
@@ -94,22 +161,23 @@ const SearchBar = styled.div<{ $isSearchBarFocused: boolean }>`
     height: 48px;
     background-color: #007be9;
     border: none;
+    & div {
+      color: white;
+    }
   }
 `;
 
 const SerachWindow = styled.div`
   background-color: white;
-  width: 100%;
   padding: 20px 0;
   border-radius: 21px;
   position: absolute;
-  width: 509px;
+  width: 530px;
   top: 315px;
   box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.1);
 `;
 
-const RecentKeywordsArea = styled.div`
-  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+const CurrentKeywordsArea = styled.div`
   padding: 0 0 24px 24px;
   & div:first-child {
     margin: 0 0 24px 0;
@@ -117,7 +185,20 @@ const RecentKeywordsArea = styled.div`
   }
 `;
 
+const CurrentKeyword = styled.div`
+  padding: 24px 0 24px 24px;
+  display: flex;
+  align-items: center;
+  & svg {
+    color: rgba(0, 0, 0, 0.2);
+    width: 21px;
+    height: 21px;
+    padding: 0 6px;
+  }
+`;
+
 const RecomendedKeywordsArea = styled.div`
+  border-top: 1px solid rgba(0, 0, 0, 0.1);
   padding: 24px 0 0 24px;
   & div:first-child {
     margin: 0 0 24px 0;
@@ -135,4 +216,17 @@ const RecomendedKeyword = styled.li`
   padding: 16px;
   margin: 0 8px 0 0;
   border-radius: 32px;
+`;
+
+const CurrentRecommendedKeywords = styled.ul``;
+const CurrentRecommendedKeyword = styled.li`
+  display: flex;
+  align-items: center;
+  padding: 0.5rem 0;
+  & svg {
+    color: rgba(0, 0, 0, 0.2);
+    width: 21px;
+    height: 21px;
+    padding: 0 6px;
+  }
 `;
